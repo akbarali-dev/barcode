be3f207ec9d970d9677db6c5de2762e13b257018
Akbarali Asqaraliyev <akbaraliasqaraliyev0610@gmail.com>
2022-06-23 13:42:14 +0500

    add swagger

--- src/main/java/uz/pdp/barcode/BarcodeApplication.java
+++ src/main/java/uz/pdp/barcode/BarcodeApplication.java
@@ -2,12 +2,11 @@ package uz.pdp.barcode;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 public class BarcodeApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(BarcodeApplication.class, args);
     }
-
 }
